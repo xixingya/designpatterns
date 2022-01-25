8114422164ad2305b7e319d008f3785d23277158
liuzhifei <2679431923@qq.com>
2022-01-25 10:41:54 +0800

    add aviator example

--- src/main/java/tech/xixing/aviator/Test.java
+++ src/main/java/tech/xixing/aviator/Test.java
@@ -2,22 +2,26 @@ package tech.xixing.aviator;
 
 import com.googlecode.aviator.AviatorEvaluator;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
         long current =System.currentTimeMillis();
-        AviatorEvaluator.getInstance().setCachedExpressionByDefault(true);
-        for (int i = 0; i < 10000000; i++) {
+        //AviatorEvaluator.getInstance().setCachedExpressionByDefault(true);
+        for (int i = 0; i < 1; i++) {
             Map<String,Object> input =new HashMap<>();
             input.put("uid",123);
+            ArrayList<Map> nums = new ArrayList<>();
+            nums.add(Collections.singletonMap("uid","qaq"));
+            input.put("nums",nums);
             HashMap<String, Object> objectObjectHashMap = new HashMap<>();
+
             objectObjectHashMap.put("value","qaq");
+
             input.put("roomId",objectObjectHashMap);
             //使用不带getInstance会使得全局开启缓存失效
-            Object execute = AviatorEvaluator.getInstance().execute("roomId.value", input);
-            //System.out.println(execute);
+            Object execute = AviatorEvaluator.getInstance().execute("#nums[0].uid", input,true);
+            System.out.println(execute);
         }
         System.out.println(System.currentTimeMillis()-current);
     }
---
+++ src/main/java/tech/xixing/common/StringTest.java
@@ -0,0 +1,42 @@
+package tech.xixing.common;
+
+import java.util.HashSet;
+
+public class StringTest {
+    public static final String qaq = "qaq";
+
+    public String getSome(){
+        return qaq+"123";
+    }
+
+    public static void main(String[] args) {
+        //int [][] nums = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
+        int [][] nums = {{}};
+        System.out.println(findNumberIn2DArray(nums, 13));
+    }
+
+    public static boolean findNumberIn2DArray(int[][] matrix, int target) {
+
+        for (int i = 0; i < matrix.length; i++) {
+            boolean range = range(matrix[i], target);
+            if(range){
+                int start = 0;
+                int end = matrix[i].length-1;
+                while (start<=end){
+                    int mid = (start+end)/2;
+                    if(matrix[i][mid]<target){
+                        start=mid+1;
+                    }else if(matrix[i][mid]>target){
+                        end = mid-1;
+                    }else {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    private static boolean range(int [] nums,int num){
+        return nums.length != 0 && nums[0] <= num && nums[nums.length - 1] >= num;
+    }
+}
