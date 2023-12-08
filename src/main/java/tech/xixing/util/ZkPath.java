a8df969c473db7788aefd87447cde289937b4a81
sheyang <sheyang@bixin.cn>
2023-12-08 19:34:16 +0800

    增加ZkPath

---
+++ src/main/java/tech/xixing/util/ZkPath.java
@@ -0,0 +1,11 @@
+package tech.xixing.util;
+
+/**
+ * Created by qinzhewudao on 2023/12/08.
+ */
+public class ZkPath {
+
+    public static final String APP_PARENT_PATH = "/dubbo/record/appList";
+    public static final String RECORD_RULE_PATH = "/dubbo/record/recordRule/%s";
+    public static final String RECORD_STATUS_PARENT_PATH = "/dubbo/record/recordStatus/%s";
+}
